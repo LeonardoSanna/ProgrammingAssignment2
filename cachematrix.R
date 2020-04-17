bb6f976fa0090c28c18c2e535e67892bc17d0aa2
Leonardo Sanna <leonardo.sanna@unimore.it>
2020-04-17 19:23:12 +0200

    Update cachematrix.R
    Programming Assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function inverts a matrix and cache the result
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invrt <- NULL #creates an empty variable to store the inverse
+        set <- function(y) { #defines the new value of the matrix in parent env
+                x <<- y
+                invrt <<- NULL
+        }
+        get <- function() x #get the value of the matrix
+        setInverse <- function() invrt <<- solve(x) #calculate the inverse and caches its value
+        getInverse <- function() invrt #gets the value of invrt
+        list(get = get, setInverse = setInverse, getInverse = getInverse) #defines scoping rules of the env
 }
 
 
-## Write a short comment describing this function
+#This function access the cache using stored values
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse() #get the stored value
+        if (!is.null(invrt)) {
+                message("getting cached data")
+                return(invrt) #if invrt is NOT null it will return a message and invrt
+        }
+        
 }
+
+
